bde1574c452c2a2c2b00c43da3bf4e613dfd8480
morphcute <evangelistakimlester@gmail.com>
2026-04-04 16:21:44 +0800

    Replace project code and fix Prisma build issue

--- checkUsers.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.user.findMany();
-  console.log("Users:", users.length, users.map(u => ({ id: u.id, email: u.email, name: u.name })));
-}
-main().finally(() => prisma.$disconnect());
--- clean.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function clean() {
-  await prisma.tournament.deleteMany({});
-  console.log("All seeded tournaments removed successfully.");
-  process.exit(0);
-}
-clean();
